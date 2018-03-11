9ab8aa54481d3fb8109e6638c07abdaefd3e0278
JiNamaMAC <josh.shephard@hotmail.com>
2018-03-11 01:02:24 -0400

    Changed server running msg
    Changed server msg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server = app.listen(3000);
 app.use(express.static('public'));
 
 if(server){
-    console.log("Server is running!");
+    console.log("Server is running! Hold 'ctrl' and press 'c' to exit.");
 }else{
     console.log("Error.");
 }
