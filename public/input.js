02bb1386afde8079e955c62951e0f224cdda4631
Josh Shephard <josh.shephard@hotmail.com>
2018-03-15 21:40:32 -0300

    fixes

--- public/input.js
+++ public/input.js
@@ -1,9 +1,16 @@
 function getInput(){
     var name = $('#inputName').val();
-    var age = $('#inputAge').val();
+    var age = Number($('#inputAge').val());
 
-    window.location = './add/' + name + '/' + age;
-    setTimeout(function(){window.location = 'localhost:3000';}, 10);
+    if (Number.isInteger(age)){
+        window.location = './add/' + name + '/' + age;
+        setTimeout(function(){window.location = 'localhost:3000';}, 10);
+        alert('Thank you for your input!\nName: ' + name + '\nAge: ' + age);
+    }else{
+        alert('Oops! Something went wrong. Please make sure you\'re inputting your age as an integer and try again.');
+    }
+    $('#inputName').val('');
+    $('#inputAge').val('');
 }
 
 function getData(){
